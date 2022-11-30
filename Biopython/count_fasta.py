3dc41c460de7fad273a017bc806a5aec29975f2b
justinboissinot <56981551+justinboissinot@users.noreply.github.com>
2022-11-30 13:13:10 -0500

    Delete Biopython directory

--- Biopython/count_fasta.py
+++
@@ -1,6 +0,0 @@
-from Bio import SeqIO
-filename = "NC_000913.faa"
-count = 0
-for record in SeqIO.parse(filename, "fasta"):
-    count = count + 1
-print("There were " + str(count) + " records in file " + filename)
--- Biopython/total_gene_lengths.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-record = SeqIO.read("NC_000913.gbk", "genbank")
-total = 0
-for feature in record.features:
-    if feature.type == "gene":
-        total = total + len(feature)
-print("Total length of all genes is " + str(total))
-
